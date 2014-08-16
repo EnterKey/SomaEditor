513b018c730eca2d8998b2cffee2a73ff059f54c
엄두성 <enterkey88@gmail.com>
2014-08-17 01:14:52 +0900

    previrew 토글 버튼 누르면 div 변경되도록 기능 추가

--- resources/javascript/editor.js
+++ resources/javascript/editor.js
@@ -13,10 +13,13 @@ $(document).ready(function() {
 editor.setEventListener = function() {
 	this.toggleTitleModal();
 	this.changeDocumentTitle();
+	this.toggleReviewDivision();
 };
 
 editor.toggleTitleModal = function() {
 	$('#writing_title').on('click', function() {
+		var title = $('#writing_title').text();
+		$('#title').val(title);
 		$('#modal-edit-writing').modal('toggle');
 	});
 };
@@ -25,5 +28,19 @@ editor.changeDocumentTitle = function() {
 	$('#btn-done').on('click', function() {
 		var title = $('#title').val();
 		$('#writing_title').html(title);
+	});
+};
+
+editor.toggleReviewDivision = function() {
+	$('#toggle_preview_btn').on('click', function() {
+		var isReviewActive = $("input:checkbox[id='toggle_preview']").is(":checked");
+		
+		if(isReviewActive) {
+			$('#editor').removeClass('col-sm-6').addClass('col-sm-12');
+			$('#preview').removeClass('col-sm-6').addClass('col-sm-12');
+		} else {
+			$('#editor').removeClass('col-sm-12').addClass('col-sm-6');
+			$('#preview').removeClass('col-sm-12').addClass('col-sm-6');
+		}
 	});
 };
